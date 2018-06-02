863e0a7aeb1c744249e35b6f0db61f53586e9942
Russell Bryan <bryanr@ou.edu>
2018-06-01 23:10:13 -0500

    new tests. All tests pass

--- test/haiku-test.lisp
+++ test/haiku-test.lisp
@@ -1,22 +1,42 @@
 (defpackage :haiku-test
  (:use :cl :prove :haiku :md :files
        :utils :templates :bindings)
- (:export))
+ (:export)
+ (:shadow :is-error))
 
 (in-package :haiku-test)
 
+(defmacro is-error (form)
+  `(prove:is-error ,form 'simple-error))
 
-(plan 2)
+(plan 3)
 
 (uiop:with-current-directory ("test/")
+  (subtest "Testing Bindings Functions"
+    (ok (bindings:symbol-equal 'utils::a 'bindings::a))
+    (let* ((bindings '((a . "1") (b . "2")))
+           (err-body-bindings (append bindings '((body . "should error"))))
+           (err-double-template-bindings (append bindings 
+                                                 '((template . "should error")
+                                                   (template . "no really"))))
+           (err-double-extends-bindings (append bindings 
+                                                 '((extends . "should error")
+                                                   (extends . "no really")))))
+      (ok (not (bindings:binding-assoc 'utils::c bindings)))
+      (ok (bindings:binding-assoc 'utils::b bindings))
+      (pass (bindings:check-bindings bindings ""))
+      (is-error (bindings:check-bindings err-body-bindings ""))
+      (is-error (bindings:check-bindings err-double-template-bindings ""))
+      (is-error (bindings:check-bindings err-double-extends-bindings "")))
+  )
   (subtest "Testing File Functions"
-    (ok "test.txt" (files:path->filename "/home/test/words/test.txt"))
-    (ok "test" (files:strip-extension 
+    (is "test.txt" (files:path->filename "/home/test/words/test.txt"))
+    (is "test" (files:strip-extension 
                  (files:path->filename "/home/test/words/test.txt")))
   )
   (subtest "Testing markdown functions"
     (diag "Eventually we need a binding error condition.")
-    (is-error (md:load-md #p"body-binding.md") 'simple-error)
+    (is-error (md:load-md #p"body-binding.md"))
 ))
 
 (finalize)
